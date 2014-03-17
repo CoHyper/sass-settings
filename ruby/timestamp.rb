9438c3f5acbfa0c30a6b4be4986dc6c77b043d78
svenlang <svenlang07@gmail.com>
2014-03-17 11:25:55 +0100

    move in one folder

--- ruby/timestamp.rb
+++ ruby/timestamp.rb
@@ -1,8 +1,21 @@
 
 module Sass::Script::Functions
 	def timestamp(string)
+		# if (string == "y") {
+			return Sass::Script::String.new(Time.now.year.to_s);
+		# }
 		
 		# use '#{timestamp()}'
-		return Sass::Script::String.new(Time.now.year.to_s);
+		
 	end
 end
+
+
+# strtotime
+# module Sass::Script::Functions
+#   def reverse(string)
+#     assert_type string, :String
+#     Sass::Script::Value::String.new(string.value.reverse);
+#   end
+#   declare :reverse, [:string]
+# end
